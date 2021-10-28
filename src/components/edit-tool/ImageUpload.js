a70ff7c7b7a8da34d83549dd714146b1eebb428c
Zachary Williams <kcaz311@gmail.com>
2021-10-28 10:52:17 -0400

    add react-hook-form

--- src/components/edit-tool/ImageUpload.js
+++ src/components/edit-tool/ImageUpload.js
@@ -1,38 +1,26 @@
-import React, {useState} from 'react'
-import ShowImage from './ShowImage'
-import defaultCoverPhoto from './images/defaultCoverPhoto.jpeg'
+import React from 'react';
+import {useForm} from 'react-hook-form'
+// import ShowImage from './ShowImage'
+// import defaultCoverPhoto from './images/defaultCoverPhoto.jpeg'
 
 const ImageUpload = () => {
-    const [image, setImage] = useState(defaultCoverPhoto)
+    // const [image, setImage] = useState(defaultCoverPhoto)
     const maxImageFileSize = 1000000
 
-    const imageSelectedHandler = event => {
-        const uploadedImage = event.target.files[0]
-        if ( uploadedImage.size > maxImageFileSize ){
-            console.log("too big")
-        }else{
-            console.log("image sent to state")
-            console.dir(uploadedImage)
-            setImage(uploadedImage)
-        }
+    const {register, handleSubmit} = useForm()
+
+    const onSubmit = (image) => {
+        console.log(image)
     }
-    const imageUploadHandler = event => {
-        console.dir(image)
-        //send image to back end
-    }
+
 
 
     return (
-    <div className="ImageUpload">
-        <ShowImage image={image}/>
-        <input 
-         style={{display: 'none'}} 
-         type="file" 
-         onChange={imageSelectedHandler}
-         ref={fileInput => this.fileInput = fileInput}/>
-        <button onClick={() => this.fileInput.click()}>Pick File</button>
-        <button onClick={imageUploadHandler}>Upload</button>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="ImageUpload">
+        {/* <ShowImage image={image}/> */}
+        <input ref={register} type='file' name='coverPhot' />
+        <button>Upload Image</button>
+    </form>
     )
 }
 
