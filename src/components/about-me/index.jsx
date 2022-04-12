c7dfa550ef5b6f77f6d83842f1b88fbed182329b
Auden Robertson <audenrobertson@gmail.com>
2022-04-12 12:40:35 -0400

    updating avatar edit css

--- src/components/about-me/index.jsx
+++ src/components/about-me/index.jsx
@@ -16,32 +16,34 @@ function AboutMe(props) {
             <div className="home-page home-page-wrapper">
 
                 <div className="welcome">
-                    <div className="cta-row container">
+                    <div className="cta_row_container">
+                        <div className='about_me_navigation_controls'></div>
+                        
                         <div className="name_and_avatar">
                             
-                        <div className="column" id="profile">
-                                <div className='column' id="avatar_edit">
-                                    <img src={avatar} alt="profile picture" />
-                                    <div className='edit'>
-                                        <a href='Change photo'>Change photo</a>
-                                        <br/>
-                                        <a href='Remove photo'>Remove current</a>
-                                    </div>                                 
-                                </div>
-                                        <div className='name_and_edit'>
-                                            <span>Gabriela Garcia</span>
-                                            <a href='edit_name'>Edit name</a>
+                            <div className="profile" >
+                                        <div className='avatar_edit'>
+                                            <img src={avatar} alt="profile picture" />
+                                            <div className='edit'>
+                                                <a href='Change photo'>Change photo</a>
+                                                <br/>
+                                                <a href='Remove photo'>Remove current</a>
+                                            </div>                                 
                                         </div>
-                                        <div className='name_and_pronouns'>
-                                            <span>She/Her/Hers</span>
-                                            <a href='edit_pronouns'>Edit</a>
-                                        </div>
-                                </div>
-                    </div>
+                                            <div className='name_and_edit'>
+                                                <span>Gabriela Garcia</span>
+                                                <a href='edit_name'>Edit name</a>
+                                            </div>
+                                            <div className='name_and_pronouns'>
+                                                <span>She/Her/Hers</span>
+                                                <a href='edit_pronouns'>Edit</a>
+                                            </div>
+                            </div>
+                        </div>
 
-                </div>
-            </div>  
-        </div>
+                    </div>
+                </div>  
+            </div>
             // <div className='nav_and_button'>Hello There</div>
         )
     }
